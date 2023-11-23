f0fb04f4957379b3c47d443298a75db6c3539e6a
werkah <wehilaszek@student.agh.edu.pl>
2023-11-23 11:03:37 +0100

    init

---
+++ length.py
@@ -0,0 +1,14 @@
+import json
+
+def get_total_length(filename):
+    total_length = 0
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            data = json.loads(line)
+            total_length += len(data)
+
+    return total_length
+
+name = 'output2.json'
+length = get_total_length(name)
+print('Total Length:', length)

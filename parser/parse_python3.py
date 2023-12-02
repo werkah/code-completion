6a5c357862bed274f6a4c7c520c4faa22ef1a4ca
dulmicha <dulmicha@gmail.com>
2023-12-02 14:37:46 +0100

    Update parser to newer python versions
    Implement match-case handling, update deprecated Num and Str handling (now: Constant), switch CLI  commands to argparse, set formatting

--- parser/parse_python3.py
+++ parser/parse_python3.py
@@ -1,141 +1,158 @@
-#!/usr/bin/python3
-
-import sys
 import json
 import ast
-
-
-def PrintUsage():
-    sys.stderr.write("""
-Usage:
-    parse_python3.py <file> <output_file>
-""")
-    exit(1)
+import argparse
 
 
 def read_file_to_string(filename):
-    with open(filename, 'rt', encoding='utf-8') as f:
+    with open(filename, "rt", encoding="utf-8") as f:
         s = f.read()
     return s
 
 
 def parse_file(filename):
-    global c, d
     tree = ast.parse(read_file_to_string(filename), filename)
 
     json_tree = []
 
-    def gen_identifier(identifier, node_type='identifier'):
+    def gen_identifier(identifier, node_type="identifier"):
         pos = len(json_tree)
         json_node = {}
         json_tree.append(json_node)
-        json_node['type'] = node_type
-        json_node['value'] = identifier
+        json_node["type"] = node_type
+        json_node["value"] = identifier
         return pos
 
-    def traverse_list(l, node_type='list'):
+    def traverse_list(l, node_type="list"):
         pos = len(json_tree)
         json_node = {}
         json_tree.append(json_node)
-        json_node['type'] = node_type
+        json_node["type"] = node_type
         children = []
         for item in l:
             children.append(traverse(item))
         if len(children) != 0:
-            json_node['children'] = children
+            json_node["children"] = children
         return pos
 
     def traverse(node):
         pos = len(json_tree)
         json_node = {}
         json_tree.append(json_node)
-        json_node['type'] = type(node).__name__
+        json_node["type"] = type(node).__name__
         children = []
 
         if isinstance(node, ast.Name):
-            json_node['value'] = node.id
-        elif isinstance(node, ast.Num):
-            json_node['value'] = str(node.n)
-        elif isinstance(node, ast.Str):
-            json_node['value'] = node.s
+            json_node["value"] = node.id
+        elif isinstance(node, ast.Constant):
+            json_node["value"] = str(node.value)
         elif isinstance(node, ast.alias):
-            json_node['value'] = str(node.name)
+            json_node["value"] = str(node.name)
             if node.asname:
                 children.append(gen_identifier(node.asname))
         elif isinstance(node, ast.FunctionDef):
-            json_node['value'] = str(node.name)
+            json_node["value"] = str(node.name)
         elif isinstance(node, ast.ClassDef):
-            json_node['value'] = str(node.name)
+            json_node["value"] = str(node.name)
         elif isinstance(node, ast.ImportFrom):
             if node.module:
-                json_node['value'] = str(node.module)
+                json_node["value"] = str(node.module)
         elif isinstance(node, ast.Global):
             for n in node.names:
                 children.append(gen_identifier(n))
         elif isinstance(node, ast.keyword):
-            json_node['value'] = str(node.arg)
+            json_node["value"] = str(node.arg)
 
         # Process children.
         if isinstance(node, ast.For):
             children.append(traverse(node.target))
             children.append(traverse(node.iter))
-            children.append(traverse_list(node.body, 'body'))
+            children.append(traverse_list(node.body, "body"))
             if node.orelse:
-                children.append(traverse_list(node.orelse, 'orelse'))
+                children.append(traverse_list(node.orelse, "orelse"))
         elif isinstance(node, ast.If) or isinstance(node, ast.While):
             children.append(traverse(node.test))
-            children.append(traverse_list(node.body, 'body'))
+            children.append(traverse_list(node.body, "body"))
             if node.orelse:
-                children.append(traverse_list(node.orelse, 'orelse'))
+                children.append(traverse_list(node.orelse, "orelse"))
         elif isinstance(node, ast.With):
             for item in node.items:
                 withitem_node = traverse(item)
                 children.append(withitem_node)
-            children.append(traverse_list(node.body, 'body'))
+            children.append(traverse_list(node.body, "body"))
+        elif isinstance(node, ast.Match):
+            children.append(traverse(node.subject))
+            children.append(traverse_list(node.cases, "cases"))
+        elif isinstance(node, ast.match_case):
+            children.append(traverse(node.pattern))
+            children.append(traverse_list(node.body, "body"))
+        elif isinstance(node, ast.MatchValue):
+            children.append(traverse(node.value))
+        elif isinstance(node, ast.MatchSingleton):
+            children.append(traverse(node.value))
+        elif isinstance(node, ast.MatchAs):
+            if node.name:
+                children.append(gen_identifier(node.name))
+            if node.pattern:
+                children.append(traverse(node.pattern))
+        elif isinstance(node, ast.MatchClass):
+            children.append(traverse(node.cls))
+        elif isinstance(node, ast.MatchMapping):
+            children.append(traverse_list(node.keys, "keys"))
+            children.append(traverse_list(node.patterns, "patterns"))
+        elif isinstance(node, ast.MatchSequence):
+            children.append(traverse_list(node.patterns, "patterns"))
+        elif isinstance(node, ast.MatchOr):
+            children.append(traverse_list(node.patterns, "patterns"))
+        elif isinstance(node, ast.MatchStar):
+            children.append(gen_identifier(node.name))
         elif isinstance(node, ast.Try):
-            children.append(traverse_list(node.body, 'body'))
-            children.append(traverse_list(node.handlers, 'handlers'))
+            children.append(traverse_list(node.body, "body"))
+            children.append(traverse_list(node.handlers, "handlers"))
             if node.orelse:
-                children.append(traverse_list(node.orelse, 'orelse'))
+                children.append(traverse_list(node.orelse, "orelse"))
             if node.finalbody:
-                children.append(traverse_list(node.finalbody, 'finalbody'))
+                children.append(traverse_list(node.finalbody, "finalbody"))
         elif isinstance(node, ast.arguments):
-            children.append(traverse_list(node.args, 'args'))
-            children.append(traverse_list(node.defaults, 'defaults'))
+            children.append(traverse_list(node.args, "args"))
+            children.append(traverse_list(node.defaults, "defaults"))
             if node.vararg:
-                children.append(gen_identifier(node.vararg, 'vararg'))
+                children.append(gen_identifier(node.vararg, "vararg"))
             if node.kwarg:
-                children.append(gen_identifier(node.kwarg, 'kwarg'))
+                children.append(gen_identifier(node.kwarg, "kwarg"))
         elif isinstance(node, ast.ExceptHandler):
             if node.type:
-                children.append(traverse_list([node.type], 'type'))
+                children.append(traverse_list([node.type], "type"))
             if node.name:
-                children.append(traverse_list([node.name], 'name'))
-            children.append(traverse_list(node.body, 'body'))
+                children.append(traverse_list([node.name], "name"))
+            children.append(traverse_list(node.body, "body"))
         elif isinstance(node, ast.ClassDef):
-            children.append(traverse_list(node.bases, 'bases'))
-            children.append(traverse_list(node.body, 'body'))
-            children.append(traverse_list(node.decorator_list, 'decorator_list'))
+            children.append(traverse_list(node.bases, "bases"))
+            children.append(traverse_list(node.body, "body"))
+            children.append(traverse_list(node.decorator_list, "decorator_list"))
         elif isinstance(node, ast.FunctionDef):
             children.append(traverse(node.args))
-            children.append(traverse_list(node.body, 'body'))
-            children.append(traverse_list(node.decorator_list, 'decorator_list'))
+            children.append(traverse_list(node.body, "body"))
+            children.append(traverse_list(node.decorator_list, "decorator_list"))
         else:
             # Default handling: iterate over children.
             for child in ast.iter_child_nodes(node):
-                if isinstance(child, ast.expr_context) or isinstance(child, ast.operator) or isinstance(child,
-                                                                                                        ast.boolop) or isinstance(
-                        child, ast.unaryop) or isinstance(child, ast.cmpop):
+                if (
+                    isinstance(child, ast.expr_context)
+                    or isinstance(child, ast.operator)
+                    or isinstance(child, ast.boolop)
+                    or isinstance(child, ast.unaryop)
+                    or isinstance(child, ast.cmpop)
+                ):
                     # Directly include expr_context, and operators into the type instead of creating a child.
-                    json_node['type'] = json_node['type'] + type(child).__name__
+                    json_node["type"] = json_node["type"] + type(child).__name__
                 else:
                     children.append(traverse(child))
 
         if isinstance(node, ast.Attribute):
-            children.append(gen_identifier(node.attr, 'attr'))
+            children.append(gen_identifier(node.attr, "attr"))
 
         if len(children) != 0:
-            json_node['children'] = children
+            json_node["children"] = children
         return pos
 
     traverse(tree)
@@ -143,13 +160,17 @@ def parse_file(filename):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        PrintUsage()
-    output_filename = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="input file")
+    parser.add_argument("output", help="output file")
+    args = parser.parse_args()
+
+    output_filename = args.output
+    input_filename = args.input
 
     try:
-        result = parse_file(sys.argv[1])
-        with open(output_filename, 'w', encoding='utf-8') as output_file:
-            json.dump(result, output_file, separators=(',', ':'), ensure_ascii=False)
+        result = parse_file(input_filename)
+        with open(output_filename, "w", encoding="utf-8") as output_file:
+            json.dump(result, output_file, separators=(",", ":"), ensure_ascii=False)
     except (UnicodeEncodeError, UnicodeDecodeError):
         pass
